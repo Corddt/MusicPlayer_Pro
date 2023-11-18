64fba6abb44e56e4a81fb19d8548ccd79610c139
Corddt <3304484088@qq.com>
2023-11-19 00:19:36 +0800

    2.0应用

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.corddt.musicplayer_pro"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
