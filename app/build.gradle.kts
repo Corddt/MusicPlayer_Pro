b35910c00e4150356ca68183bd3576d13618919f
Corddt <3304484088@qq.com>
2023-11-21 12:10:59 +0800

    3.0对界面的展示逻辑进行了重新优化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 2
-        versionName = "2.0"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
