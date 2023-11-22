385a207d99ec141d5ef69a18262e77ae99eba74f
Corddt <3304484088@qq.com>
2023-11-22 18:14:20 +0800

    3.1，增加了一些歌曲

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.corddt.musicplayer_pro"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "3.0"
+        versionCode = 3
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
